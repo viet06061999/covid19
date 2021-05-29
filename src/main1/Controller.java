0e7a8ff25d690665cf67b889f67b01f58eec2714
VietNB <vietlan2511@gmail.com>
2021-05-30 03:50:10 +0700

    update wwith excel

--- src/main1/Controller.java
+++ src/main1/Controller.java
@@ -1,30 +1,149 @@
 package main1;
 
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.geometry.Rectangle2D;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
+import main1.dao.user.UserDAO;
+import main1.entity.db.User;
+import main1.utils.ExelUtil;
 
+import java.awt.*;
 import java.io.IOException;
+import java.net.URL;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.List;
+import java.util.ResourceBundle;
 
-public class Controller {
+public class Controller implements Initializable {
 
     @FXML
     Button themDuLieu;
 
+    @FXML
+    TableView<User> myTableView;
+
+    @FXML
+    TableColumn<User, String> maDonVi;
+    @FXML
+    TableColumn<User, String> maNgay;
+    @FXML
+    TableColumn<User, String> maBan;
+    @FXML
+    TableColumn<User, String> maMauBenhPham;
+    @FXML
+    TableColumn<User, String> hinhThucLayMau;
+    @FXML
+    TableColumn<User, Integer> maThuTu;
+    @FXML
+    TableColumn<User, String> hoVaTen;
+    @FXML
+    TableColumn<User, String> xaNoiOHienTai;
+    @FXML
+    TableColumn<User, String> thonNoiOHienTai;
+    @FXML
+    TableColumn<User, String> ngheNghiep;
+    @FXML
+    TableColumn<User, String> noiLamViecHocTap;
+    @FXML
+    TableColumn<User, String> doiTuongLayMau;
+    @FXML
+    TableColumn<User, String> lanLayMau;
+    @FXML
+    TableColumn<User, String> ghiChuNeuCoTruongHopDacBiet;
+    @FXML
+    TableColumn<User, String> phanLoaiNoiLayMau;
+    @FXML
+    TableColumn<User, String> diaDiemNoiLayMau;
+    @FXML
+    TableColumn<User, String> huyenNoiLayMau;
+    @FXML
+    TableColumn<User, String> xaNoiLayMau;
+    @FXML
+    TableColumn<User, String> thonNoiLayMau;
+    @FXML
+    TableColumn<User, String> loaiMau;
+    @FXML
+    TableColumn<User, String> donViLayMau;
+    @FXML
+    TableColumn<User, String> maNguoiDuocLayMau;
+    @FXML
+    TableColumn<User, String> ngayLayMau;
+    @FXML
+    TableColumn<User, String> loaiGop;
+    @FXML
+    TableColumn<User, String> ngayXetNghiem;
+    @FXML
+    TableColumn<User, String> phuongPhapXetNghiem;
+    @FXML
+    TableColumn<User, String> ngayTraKetQuaXetNghiem;
+    @FXML
+    TableColumn<User, String> donViGuiMau;
+    @FXML
+    TableColumn<User, String> tinhTrangMau;
+    @FXML
+    TableColumn<User, String> ketQuaXetNghiem;
+    @FXML
+    TableColumn<User, String> ctValue;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        maDonVi.setCellValueFactory(new PropertyValueFactory<User, String>("maDonVi"));
+        maNgay.setCellValueFactory(new PropertyValueFactory<User, String>("maNgay"));
+        maBan.setCellValueFactory(new PropertyValueFactory<User, String>("maBan"));
+        maMauBenhPham.setCellValueFactory(new PropertyValueFactory<User, String>("maMauBenhPham"));
+        hinhThucLayMau.setCellValueFactory(new PropertyValueFactory<User, String>("hinhThucLayMau"));
+        maThuTu.setCellValueFactory(new PropertyValueFactory<User, Integer>("maThuTu"));
+        hoVaTen.setCellValueFactory(new PropertyValueFactory<User, String>("hoTen"));
+        xaNoiOHienTai.setCellValueFactory(new PropertyValueFactory<User, String>("xa"));
+        thonNoiOHienTai.setCellValueFactory(new PropertyValueFactory<User, String>("thon"));
+        ngheNghiep.setCellValueFactory(new PropertyValueFactory<User, String>("noiLamViec"));
+        noiLamViecHocTap.setCellValueFactory(new PropertyValueFactory<User, String>("noiLamViec"));
+        doiTuongLayMau.setCellValueFactory(new PropertyValueFactory<User, String>("doiTuongLayMau"));
+        lanLayMau.setCellValueFactory(new PropertyValueFactory<User, String>("lanLayMau"));
+        ghiChuNeuCoTruongHopDacBiet.setCellValueFactory(new PropertyValueFactory<User, String>("ghiChu"));
+        phanLoaiNoiLayMau.setCellValueFactory(new PropertyValueFactory<User, String>("phanLoaiNoiLayMau"));
+        diaDiemNoiLayMau.setCellValueFactory(new PropertyValueFactory<User, String>("diaDiemNoiLayMau"));
+        huyenNoiLayMau.setCellValueFactory(new PropertyValueFactory<User, String>("huyenLayMau"));
+        xaNoiLayMau.setCellValueFactory(new PropertyValueFactory<User, String>("xaLayMau"));
+        thonNoiLayMau.setCellValueFactory(new PropertyValueFactory<User, String>("thonLayMau"));
+        loaiMau.setCellValueFactory(new PropertyValueFactory<User, String>("loaiMau"));
+        donViLayMau.setCellValueFactory(new PropertyValueFactory<User, String>("donViLayMau"));
+        maNguoiDuocLayMau.setCellValueFactory(new PropertyValueFactory<User, String>("maNguoiDuocLayMau"));
+        ngayLayMau.setCellValueFactory(new PropertyValueFactory<User, String>("ngayLayMau"));
+        ngayXetNghiem.setCellValueFactory(new PropertyValueFactory<User, String>("ngayXetNghiem"));
+        phuongPhapXetNghiem.setCellValueFactory(new PropertyValueFactory<User, String>("phuongPhapXetNghiem"));
+        ngayTraKetQuaXetNghiem.setCellValueFactory(new PropertyValueFactory<User, String>("ngayTraKetQua"));
+        donViGuiMau.setCellValueFactory(new PropertyValueFactory<User, String>("donViGuiMau"));
+        tinhTrangMau.setCellValueFactory(new PropertyValueFactory<User, String>("tinhTrangMau"));
+        ketQuaXetNghiem.setCellValueFactory(new PropertyValueFactory<User, String>("ketQuaXetNghiem"));
+        ctValue.setCellValueFactory(new PropertyValueFactory<User, String>("ctValue"));
+        bindTableView(UserDAO.getInstance().getUsers());
+    }
+
+    void bindTableView(List<User> users) {
+        ObservableList list = FXCollections.observableArrayList(users);
+        myTableView.setItems(list);
+    }
+
     public void timKiemAction(ActionEvent event) {
         System.out.println("Button Clicked!");
 
-        Date now= new Date();
+        Date now = new Date();
 
         DateFormat df = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss.SSS");
 
@@ -36,26 +155,18 @@ public class Controller {
 
     }
 
-
     public void xuatExcelAction(ActionEvent event) {
-        System.out.println("Button Clicked!");
-
-        Date now= new Date();
-
-        DateFormat df = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss.SSS");
-
-        // Dữ liệu Model
-        String dateTimeString = df.format(now);
-
-        // Hiển thị lên VIEW.
-        //myTextField.setText(dateTimeString);
-
+        try {
+            ExelUtil.export(UserDAO.getInstance().getUsers(), myTableView);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public void themDuLieuAction(ActionEvent event) {
         Stage stage = new Stage();
         Parent myNewScene = null;
-        if (event.getSource() == themDuLieu){
+        if (event.getSource() == themDuLieu) {
             //stage = (Stage) themDuLieu.getScene().getWindow();
             try {
                 myNewScene = FXMLLoader.load(getClass().getResource("insert.fxml"));
--- src/main1/utils/ExelUtil.java
+++ src/main1/utils/ExelUtil.java
@@ -2,14 +2,17 @@ package main1.utils;
 
 import com.gembox.spreadsheet.ExcelFile;
 import com.gembox.spreadsheet.ExcelWorksheet;
+import com.gembox.spreadsheet.SpreadsheetInfo;
 import javafx.collections.ObservableList;
 import javafx.scene.control.TableView;
 import javafx.stage.FileChooser;
+import main1.entity.db.User;
 
 import java.io.File;
 import java.io.IOException;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.List;
 import java.util.logging.Logger;
 
 /**
@@ -17,15 +20,82 @@ import java.util.logging.Logger;
  */
 public class ExelUtil {
 
-	public void export(TableView table) throws IOException {
+	public static void export(List<User> userList, TableView table) throws IOException {
+		SpreadsheetInfo.setLicense("FREE-LIMITED-KEY");
 		ExcelFile file = new ExcelFile();
 		ExcelWorksheet worksheet = file.addWorksheet("sheet");
-		for (int row = 0; row < table.getItems().size(); row++) {
-			ObservableList cells = (ObservableList) table.getItems().get(row);
-			for (int column = 0; column < cells.size(); column++) {
-				if (cells.get(column) != null)
-					worksheet.getCell(row, column).setValue(cells.get(column).toString());
-			}
+		worksheet.getCell(0, 0).setValue("Mã Đơn vị");
+		worksheet.getCell(0, 1).setValue("Mã ngày");
+		worksheet.getCell(0, 2).setValue("Mã bàn");
+		worksheet.getCell(0, 3).setValue("Hình thức lấy mẫu*");
+		worksheet.getCell(0, 4).setValue("Mã TT");
+		worksheet.getCell(0, 5).setValue("     Họ và Tên   ");
+		worksheet.getCell(0, 6).setValue("Năm sinh");
+		worksheet.getCell(0, 7).setValue("Giới tính");
+		worksheet.getCell(0, 8).setValue("Số điện thoại");
+		worksheet.getCell(0, 9).setValue("Huyện (nơi ở hiện tại)");
+		worksheet.getCell(0, 10).setValue("Xã (nơi ở hiện tại)");
+		worksheet.getCell(0, 11).setValue("Thôn (nơi ở hiện tại)");
+		worksheet.getCell(0, 12).setValue("Nghề nghiệp");
+		worksheet.getCell(0, 13).setValue("Nơi làm việc, học tập");
+		worksheet.getCell(0, 14).setValue("Đối tượng lấy mẫu");
+		worksheet.getCell(0, 15).setValue("Lần lấy mẫu");
+		worksheet.getCell(0, 16).setValue("Ghi chú (nếu có trường hợp đặc biệt)");
+		worksheet.getCell(0, 17).setValue("Phân loại nơi lấy mẫu");
+		worksheet.getCell(0, 18).setValue("Địa điểm nơi lấy mẫu");
+		worksheet.getCell(0, 19).setValue("Huyện (nơi lấy mẫu)");
+		worksheet.getCell(0, 20).setValue("Xã (nơi lấy mẫu)");
+		worksheet.getCell(0, 21).setValue("Thôn (nơi lấy mẫu)");
+		worksheet.getCell(0, 22).setValue("Loại mẫu");
+		worksheet.getCell(0, 23).setValue("Đơn vị lấy mẫu");
+		worksheet.getCell(0, 24).setValue("Mã mẫu bệnh phẩm*");
+		worksheet.getCell(0, 25).setValue("Mã người được lấy mẫu*");
+		worksheet.getCell(0, 26).setValue("Ngày lấu mẫu*");
+		worksheet.getCell(0, 27).setValue("Loại gộp");
+		worksheet.getCell(0, 28).setValue("Ngày xét nghiệm");
+		worksheet.getCell(0, 29).setValue("Phương pháp xét nghiệm");
+		worksheet.getCell(0, 30).setValue("Ngày trả KQXN");
+		worksheet.getCell(0, 31).setValue("Đơn vị gửi mẫu");
+		worksheet.getCell(0, 32).setValue("Tình trạng mẫu");
+		worksheet.getCell(0, 33).setValue("Kết quả xét nghiệm");
+		worksheet.getCell(0, 34).setValue("CT Value");
+		for (int row = 0; row < userList.size(); row++) {
+			User user = userList.get(row);
+			worksheet.getCell(row+1, 0).setValue(user.getMaDonVi());
+			worksheet.getCell(row+1, 1).setValue(user.getMaNgay());
+			worksheet.getCell(row+1, 2).setValue(user.getMaBan());
+			worksheet.getCell(row+1, 3).setValue(user.getMaMauBenhPham());
+			worksheet.getCell(row+1, 4).setValue(user.getHinhThucLayMau());
+			worksheet.getCell(row+1, 5).setValue(user.getStt());
+			worksheet.getCell(row+1, 6).setValue(user.getHoTen());
+			worksheet.getCell(row+1, 7).setValue(user.getNamSinh());
+			worksheet.getCell(row+1, 8).setValue(user.getGioiTinh());
+			worksheet.getCell(row+1, 9).setValue(user.getSdt());
+			worksheet.getCell(row+1, 10).setValue(user.getHuyen());
+			worksheet.getCell(row+1, 11).setValue(user.getXa());
+			worksheet.getCell(row+1, 12).setValue(user.getThon());
+			worksheet.getCell(row+1, 13).setValue(user.getNgheNghiep());
+			worksheet.getCell(row+1, 14).setValue(user.getNoiLamViec());
+			worksheet.getCell(row+1, 15).setValue(user.getDoiTuongLayMau());
+			worksheet.getCell(row+1, 16).setValue(user.getLanLaymau());
+			worksheet.getCell(row+1, 17).setValue(user.getGhiChu());
+			worksheet.getCell(row+1, 18).setValue(user.getPhanLoaiNoiLayMau());
+			worksheet.getCell(row+1, 19).setValue(user.getDiaDiemNoiLaymau());
+			worksheet.getCell(row+1, 20).setValue(user.getHuyenLayMau());
+			worksheet.getCell(row+1, 21).setValue(user.getXaLayMau());
+			worksheet.getCell(row+1, 22).setValue(user.getThonLayMau());
+			worksheet.getCell(row+1, 23).setValue(user.getLoaiMau());
+			worksheet.getCell(row+1, 24).setValue(user.getDonViLayMau());
+			worksheet.getCell(row+1, 25).setValue(user.getMaNguoiDuocLayMau());
+			worksheet.getCell(row+1, 26).setValue(user.getNgayLayMau());
+			worksheet.getCell(row+1, 27).setValue(user.getLoaiGop());
+			worksheet.getCell(row+1, 28).setValue(user.getNgayXetNghiem());
+			worksheet.getCell(row+1, 29).setValue(user.getPhuongPhapXetNghiem());
+			worksheet.getCell(row+1, 30).setValue(user.getNgayTraKetQua());
+			worksheet.getCell(row+1, 31).setValue(user.getDonViGuiMau());
+			worksheet.getCell(row+1, 32).setValue(user.getTinhTrangMau());
+			worksheet.getCell(row+1, 33).setValue(user.getKetQuaXetNghiem());
+			worksheet.getCell(row+1, 34).setValue(user.getCtValue());
 		}
 
 		FileChooser fileChooser = new FileChooser();
